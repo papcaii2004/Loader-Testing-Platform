34de8d718366c9232cd037bb807f8760a37acfb4
papcaii <duyhn2004.work@gmail.com>
2026-03-06 23:52:13 +0700

    improve debug log

--- src/techniques/4_writing/write_local.h
+++ src/techniques/4_writing/write_local.h
@@ -16,8 +16,8 @@ BOOL Stage4_Write_Local(LPVOID dest, unsigned char* src, int len) {
         DEBUG_MSG("Stage 4", "Write completed");
         unsigned char* check = (unsigned char*)dest;
         DEBUG_MSG("Stage 4",
-          "Dest bytes: %02X %02X %02X %02X",
-          check[0], check[1], check[2], check[3]);
+          "Dest bytes: %02X %02X %02X %02X %02X %02X %02X %02X",
+          check[0], check[1], check[2], check[3], check[4], check[5], check[6], check[7]);
     #endif
 
     return TRUE;
