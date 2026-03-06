e5fc127e57ef18bde7a2e9ce15d1d94cee27e341
papcaii <duyhn2004.work@gmail.com>
2026-03-06 23:51:57 +0700

    Update exec_thread.h

--- src/techniques/5_execution/exec_thread.h
+++ src/techniques/5_execution/exec_thread.h
@@ -3,6 +3,7 @@
 #include "../../core/utils.h"
 
 void Stage5_Exec_LocalThread(LPVOID address) {
+    DEBUG_MSG("Stage 5", "Execute on new thread");
     HANDLE hThread = MyCreateThread(address);
     if (hThread) {
         MyWaitForSingleObject(hThread);
